397ed829e49438c5c5f85f425f2ced6e634f41fa
k-yoshino-bs <81940521+k-yoshino-bs@users.noreply.github.com>
2021-07-25 23:13:44 +0900

    アプリちょっと作成

--- src/index.js
+++ src/index.js
@@ -1,188 +1,24 @@
-/**
- * const、left等の変数宣言
- */
-// var val1 = "var変数";
-// console.log(val1);
+import "./styles.css";
 
-// // var変数は上書き可能
-// val1 = "var変数を上書き";
-// console.log(val1);
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
 
-// // var変数は再宣言可能
-// var val1 = "var変数を再宣言";
-// console.log(val1);
+  // divタグ生成
+  const div = document.createElement("div");
+  div.className = "list-row";
 
-// let val2 = "let変数";
-// console.log(val2);
+  //liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
 
-// // left変数は上書きが可能
-// val2 = "let変数を上書き";
-// console.log(val2);
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
 
-// letは再宣言不可能
-//let val2 = "let変数を再宣言";
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
 
-// const val4 = {
-//   name: "よしの",
-//   age: 24,
-// };
-// val4.name = "yoshi";
-// val4.address = "tokyo";
-// console.log(val4);
-
-// const配列で定義した値は上書き可能
-// const val5 = ["dog", "cat"];
-// val5[0] = "brid";
-// val5.push("monkey");
-// console.log(val5);
-
-// /**
-//  * テンプレート文字列
-//  */
-
-// const name = "よしの";
-// const age = 24;
-
-// // 従来の方法での出力
-// const message1 = "私の名前は" + name + "です。年齢は、" + age + "です。";
-// console.log(message1);
-
-// // テンプレート文字列を用いた方法
-// const message2 = `私の名前は${name}です。年齢は、${age}です。`;
-// console.log(message2);
-
-// /**
-//  * アロー関数
-//  */
-// //従来の関数
-// const func1 = function (str) {
-//   return str;
-// };
-// console.log(func1("func1です。"));
-
-// //アロー関数
-// const func2 = (str) => {
-//   return str;
-// };
-// console.log(func2("func2です。"));
-
-// const func3 =  (num1, num2) => {
-//   return num1 + num2;
-// };
-// console.log(func3(10,20));
-
-/**
- * 分割代入
- */
-// const myProfile = {
-//   name: "よしの",
-//   age: 25
-// };
-// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
-// console.log(message1);
-
-// const { name, age } = myProfile;
-// const message2 = `名前は${name}です。年齢は${age}です。`;
-// console.log(message2);
-
-// const myProfile = ["よしの", 24];
-// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
-// console.log(message3);
-
-// const [name, age] = myProfile;
-// const message4 = `名前は${name}です。年齢は${age}です。`;
-// console.log(message4);
-
-/**
- * デフォルト値、引数など
- */
-// const sayHello = (name = "ゲスト") => console.log(`こんにちは！${name}さん！`);
-// sayHello("よしの");
-
-/**
- * スプレッド構文 ...
- */
-// 配列の展開
-// const arr1 = [1, 2];
-// console.log(arr1);
-// console.log(...arr1);
-
-// const sumFunc = (num1, num2) => console.log(num1 + num2);
-// sumFunc(arr1[0], arr1[1]);
-// // スプレッド構文を資料すると↑と同じ出力結果になる
-// sumFunc(...arr1);
-
-// まとめる
-// const arr2 = [1,2,3,4,5];
-// const [num1,num2, ...arr3] = arr2;
-// console.log(num1);
-// console.log(num2);
-// console.log(arr3);
-
-// 配列のコピー、結合
-// const arr4 = [10, 20];
-// const arr5 = [30, 40];
-
-// const arr6 = [...arr4];
-// arr6[0] = 100;
-// console.log(arr4);
-
-// const arr7 = [...arr4, ...arr5];
-// console.log(arr7);
-
-// 配列のコピーはこれを使わない
-// const arr8 = arr4;
-// console.log(arr8);
-// arr8[0] = 100;
-// console.log(arr4);
-
-/**
- * mapやfilterを使った配列の処理
- */
-//const nameArr = ["田中", "山田", "吉野"];
-// 従来の方法
-// for(let index = 0;index < nameArr.length; index++){
-//   console.log(nameArr[index]);
-// }
-
-// mapの使い方
-// const nameArr2 = nameArr.map((name) => {
-//   return name;
-// })
-// console.log(nameArr2);
-
-// 1つずつ要素を操作できる
-// nameArr.map((name, index) => console.log(`${index}番目は${name}です。`));
-
-// const numArr = [1,2,3,4,5];
-// const newNumArr = numArr.filter((num) => {
-//   return num % 2 === 1;
-// });
-// console.log(newNumArr);
-
-// const newNameArr = nameArr.map((name) => {
-//   if(name === "吉野"){
-//     return name;
-//   }else{
-//     return `${name}さん`;
-//   }
-// })
-// console.log(newNameArr);
-
-/**
- * 三項演算子
- */
-// ある条件 ? 条件がtrueのとき : 条件がfalseのとき
-// const val1 = 1 > 0 ? 'trueです。' : 'falseです。';
-// console.log(val1);
-
-// const num = 1300;
-// console.log(num.toLocaleString());
-
-// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください';
-// console.log(formattedNum);
-
-// const checkSum = (num1, num2) => {
-//   return num1 + num2 > 100 ? "100を超えています！" : "許容範囲内です。";
-// };
-// console.log(checkSum(10, 91));
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
